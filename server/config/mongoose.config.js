6c1a09b2dfd8e5be675b07fd1229bf8bc8444e5b
Zak Kranis <zaksinark@gmail.com>
2023-02-28 14:49:19 -0500

    test

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true)
 
+// test comment
 mongoose.connect('mongodb://127.0.0.1:27017/Mustache_DB', {
     useNewUrlParser: true,
     useUnifiedTopology: true
